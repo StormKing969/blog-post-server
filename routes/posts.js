61c9ad8057257a97306f7704113075bb2d3138d3
StormKing969 <wijesinghesaj@gmail.com>
2022-04-25 14:40:08 -0400

    heroku fix

--- routes/posts.js
+++ routes/posts.js
@@ -6,6 +6,6 @@ const router = express.Router();
 router.get("/", getPosts);
 router.post("/", createPost);
 
-router.get("/:topic", getSinglePost);
+router.get(":topic", getSinglePost);
 
 export default router;
